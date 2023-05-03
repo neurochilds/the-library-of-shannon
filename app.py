d4ab080113a6f33a4debbab7a2d72cbaa348ac9b
Tom Childs <childsy123@gmail.com>
2023-05-03 17:44:13 +0100

    Rewrote using FastAPI instead of Flask

--- app.py
+++ app.py
@@ -1,15 +1,80 @@
-from flask import Flask, render_template, request, jsonify
-import random
+from fastapi import FastAPI, WebSocket, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import json
+import random
+import time
+import asyncio
 
-app = Flask(__name__)
+old_time = time.time()
 
-global stop_requested 
-global params
-global state
-global selection
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-state = {
+state = {}
+params = {}
+stop_requested = False
+
+TEXT_FILE = 'source_text.txt'
+WORD_DICTIONARY = 'words_dictionary.json'
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    data = await websocket.receive_json()
+    book = data["book"]
+    words = data["words"]
+    order = data["order"]
+    await generate_words(websocket, book=book, words=words, order=order)
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('index.html', {"request": request})
+
+
+@app.get('/info.html', response_class=HTMLResponse)
+async def info(request: Request):
+    return templates.TemplateResponse('info.html', {"request": request})
+
+
+async def generate_words(websocket: WebSocket, book, words, order):
+    global stop_requested
+    stop_requested = False
+
+    if not (book and words and order):
+        return await send_error_message(websocket, "Please fill in all fields.")
+    
+    try:
+        book = int(book)
+        words = int(words)
+        order = int(order)
+        if (book < 1) or (words < 1) or (order < 0) or (order > 4):
+            return await send_error_message(websocket, "Book and words must be a positive integer. Order must be an integer between 0-4.")
+    except ValueError:
+        return await send_error_message(websocket, "Book and words must be a positive integer. Order must be an integer between 0-4.")
+    
+    dictionary = []
+    with open(WORD_DICTIONARY, 'r') as file:
+        dictionary = list(json.load(file).keys())
+
+    global params
+    params = {
+        'text': await load_text(TEXT_FILE), 
+        'max_searches': 500000, 
+        'noise': 0, 
+        'rand': random.Random(), 
+        'noise_rand': random.Random(),
+        'sentence_enders': ['.', '!', '?'],
+        'max_order_of_approx': order,
+        'max_words': words,
+        'type_delay': 0.025,
+        'book': book
+        }
+    
+    global state
+    state = {
         'first_word': True,
         'n_words': 0,
         'words': [0, 0, 0, 0],
@@ -18,151 +83,90 @@ state = {
         'ends_in_comma': False,
         'curr_order_of_approx': 1,
         'sentence': '',
-        'finished_generating': True
-    }
+        'finished_generating': False
+        }
 
-selection = {'book':'', 'words':'', 'order':''}
-    
-@app.route("/")
-def index():
-    return render_template('index.html')
+    params['rand'].seed(book)
+    params['noise_rand'].seed(book)
 
+    if params['max_order_of_approx'] == 0:
+        await construct_random_text(websocket, params=params, state=state, dictionary=dictionary)
+    elif params['max_order_of_approx'] == 1:
+        await construct_text(websocket, params=params, state=state, is_first_order=True)
+    else:
+        await construct_text(websocket, params=params, state=state)
 
-@app.route('/info.html', methods=['GET'])
-def info():
-    return render_template('info.html')
-
-
-@app.route('/fetch_generated_text', methods=['GET'])
-def fetch_generated_text():
-    return jsonify(generated_text=state['sentence'], finished_generating=state['finished_generating'], book=selection['book'], order=selection['order'])
+    return {'status': 'success'}
 
 
-@app.route("/generate_words", methods=["POST"])
-def generate_words():
-    if request.method == "POST":
-        global stop_requested 
-        stop_requested = False
-        book = request.form.get("book")
-        words = request.form.get("words")
-        order = request.form.get("order")
-
-        global selection
-        selection = {'book':book, 'words':words, 'order':order}
-
-        if not (book and words and order):
-            return apology("Please fill in all fields.")
-        
-        try:
-            book = int(book)
-            if book < 1:
-                return apology("Book must be a positive integer.")
-        except:
-            return apology("Book must be a positive integer.")
-        
-        try:
-            words = int(words)
-            if words < 1:
-                return apology("Words must be a positive integer.")
-        except:
-            return apology("Words must be a positive integer.")
-        
-        try:
-            order = int(order) 
-            if order < 0 or order > 4:
-                return apology("Order must be an integer between 0-4.")
-        except:
-            return apology("Order must be an integer between 0-4.")
-        
-        text_file = 'source_text.txt'
-        word_dictionary = 'words_dictionary.json'
-        dictionary = []
-        with open(word_dictionary, 'r') as file:
-            dictionary = list(json.load(file).keys())
-
-        global params
-        params = {
-            'text': load_text(text_file), 
-            'max_searches': 500000, 
-            'noise': 0, 
-            'rand': random.Random(), 
-            'noise_rand': random.Random(),
-            'sentence_enders': ['.', '!', '?'],
-            'max_order_of_approx': order,
-            'max_words': words,
-            'type_delay': 0.025
-            }
-        
-        global state
-        state = {
-            'first_word': True,
-            'n_words': 0,
-            'words': [0, 0, 0, 0],
-            'sentences': 0,
-            'sentence_ended': False,
-            'ends_in_comma': False,
-            'curr_order_of_approx': 1,
-            'sentence': '',
-            'finished_generating': False
-            }
-
-        params['rand'].seed(book)
-        params['noise_rand'].seed(book)
-
-        if params['max_order_of_approx'] == 0:
-            print_random(params=params, state=state, dictionary=dictionary)
-        elif params['max_order_of_approx'] == 1:
-            print_text(params=params, state=state, selection=selection, is_first_order=True)
-        else:
-            print_text(params=params, state=state, selection=selection)
-    return "", 204
+async def send_error_message(websocket: WebSocket, message: str):
+    if websocket is not None:
+        return await websocket.send_json({'message': message})
 
 
-def apology(message):
-    return render_template("apology.html", message=message)
-
-
-def load_text(folder_path):
+async def load_text(folder_path):
     with open(f'{folder_path}', 'r', encoding='utf-8') as file:
         return file.read().split()
     
 
-def print_text(params, state, selection, is_first_order=False):
+async def construct_text(websocket: WebSocket, params, state, is_first_order=False):
     while state['n_words'] < params['max_words'] and not stop_requested:
         if is_first_order:
-            construct_first_order_sentence(params=params, state=state, selection=selection)
+            await construct_first_order_sentence(websocket, params=params, state=state)
         else:
-            construct_markov_sentence(params=params, state=state, selection=selection)
+            await construct_markov_sentence(websocket, params=params, state=state)
 
         if stop_requested:
             return
-        elif state['n_words'] < params['max_words']:
+
+        if state['n_words'] < params['max_words']:
             if state['sentences'] > params['rand'].randint(5, 20):
-                state['sentence'] += '<br><br>' # New para
+                state['sentence'] += '<<' # New para. JS code will conver '<' to '<br>'
                 state['sentences'] = 0
+
     state['finished_generating'] = True
 
+    return await send_text_to_client(websocket, state=state, params=params) 
 
-def print_random(dictionary, params, state):
+
+async def construct_random_text(websocket: WebSocket, state, params, dictionary):
     while state['n_words'] < params['max_words'] and not stop_requested:
         index = params['rand'].randint(0, len(dictionary) - 1)
         new_word = dictionary[index]
         if state['first_word']:
             new_word = new_word.capitalize()
             state['first_word'] = False
-        state['sentence'] += add_noise(new_word, params['noise'], params['noise_rand'])
+        state['sentence'] += await add_noise(new_word, params['noise'], params['noise_rand'])
         state['n_words'] += 1
         
         if state['n_words'] == params['max_words']:
             state['sentence'] += '.'
-        elif params['rand'].randint(0, 20) > 19:
-            state['sentence'] += '.<br><br>' # New para
-            state['first_word'] = True
+        elif params['rand'].randint(0, 15) > 14:
+            if state['sentences'] > params['rand'].randint(5, 20):
+                state['sentence'] += '.<<' # New para. JS code will conver '<' to '<br>'
+                state['sentences'] = 0
+                state['first_word'] = True
+            else:
+                state['sentence'] += '. '
+                state['sentences'] += 1 
+                state['first_word'] = True
         else:
             state['sentence'] += ' '
 
+        global old_time
+        if time.time() - old_time > 1: 
+            old_time = time.time()
+            await send_text_to_client(websocket, state=state, params=params) 
+    
+    state['finished_generating'] = True
 
-def construct_first_order_sentence(params, state, selection):
+    if await is_stop_requested(websocket):
+        return
+    
+    return await send_text_to_client(websocket, state=state, params=params) 
+    
+
+async def construct_first_order_sentence(websocket: WebSocket, params, state):
     state['sentence_ended'] = False
     state['first_word'] = True
     text = params['text']
@@ -187,10 +191,10 @@ def construct_first_order_sentence(params, state, selection):
             state['sentence_ended'] = True
             state['sentences'] += 1
 
-        add_word_ending_and_noise(new_word, state=state, params=params)
+        await add_word_ending_and_noise(websocket, new_word, state=state, params=params)
 
 
-def construct_markov_sentence(state, params, selection):
+async def construct_markov_sentence(websocket: WebSocket, state, params):
     text = params['text']
     state['sentence_ended'] = False
     while not state['sentence_ended']:
@@ -271,38 +275,57 @@ def construct_markov_sentence(state, params, selection):
                     state['words'][params['max_order_of_approx'] -1] = 0
                     state['curr_order_of_approx'] -= 1
                 
-                add_word_ending_and_noise(new_word, state=state, params=params)
+                await add_word_ending_and_noise(websocket, new_word, state=state, params=params)
 
 
-def add_word_ending_and_noise(word, state, params):
+async def add_word_ending_and_noise(websocket: WebSocket, word, state, params):
+    if await is_stop_requested(websocket):
+        return
+    
     if state['n_words'] == params['max_words']:
         if word[-1] not in params['sentence_enders']:
             if word[-1] == ',':
-                state['sentence'] += add_noise(word[:-1], params['noise'], params['noise_rand']) + '.'
+                state['sentence'] += await add_noise(word[:-1], params['noise'], params['noise_rand']) + '.'
             else:
-                state['sentence'] += add_noise(word, params['noise'], params['noise_rand']) + '.'
+                state['sentence'] += await add_noise(word, params['noise'], params['noise_rand']) + '.'
         else:
-            state['sentence'] += add_noise(word, params['noise'], params['noise_rand'])
+            state['sentence'] += await add_noise(word, params['noise'], params['noise_rand'])
     else:
-        state['sentence'] += add_noise(word, params['noise'], params['noise_rand']) + ' '
+        state['sentence'] += await add_noise(word, params['noise'], params['noise_rand']) + ' '
+
+   # If last update was sent more than 1 second ago, send updated text to client via WebSocket
+    global old_time
+    if time.time() - old_time > 1:
+        old_time = time.time()
+        return await send_text_to_client(websocket, state=state, params=params) 
+    return
 
 
-def add_noise(word, noise, noise_rand):
+async def send_text_to_client(websocket:WebSocket, state, params):
+    if websocket is not None:
+        return await websocket.send_json({'generated_text': state['sentence'], 'book': params['book'], 'order': params['max_order_of_approx'], 'finished_generating': state['finished_generating']})
+
+
+async def add_noise(word, noise, noise_rand):
     if noise:
         word = ''.join(c if noise_rand.randint(0, 99) > noise else '*' for c in word)
     return word
 
 
-@app.route('/stop_generating_words', methods=['GET'])
-def stop_generating_words():
-    global stop_requested
-    stop_requested = True
-    state['finished_generating'] = True
-    return 'Stopped generating words'
+async def is_stop_requested(websocket: WebSocket):
+    try:
+        data = await asyncio.wait_for(websocket.receive_json(), timeout=.1)
+        if "stop" in data and data["stop"]:
+            await websocket.close()
+            global stop_requested
+            stop_requested = True
+            return True
+    except asyncio.TimeoutError:
+        return False
 
 
-@app.route('/reset_words', methods=['POST'])
-def reset_words():
+@app.post('/reset_words')
+async def reset_words():
     global stop_requested
     stop_requested = True
     global state
